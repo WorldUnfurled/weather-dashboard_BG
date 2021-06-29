ffb7d08b1bdc0e143d0fcab20623122f1397283f
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-06-28 22:42:57 -0400

    Linked html to JS; Added JS function that fetches api data and returns response.json

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
 var key = '4df499f07bd89e71ed347810c57fb5b1';
+var city;
+var requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely&appid=" + key;
+
+
+var n = function() {
+    fetch(requestURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+n();
