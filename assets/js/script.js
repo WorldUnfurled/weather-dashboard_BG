52ff12517953ace1ea189f5d3d7e779cfea71ffe
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-06-28 22:54:34 -0400

    Added functionality to data function that allows it to forecast 6 days

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,9 @@ var getForecast = function() {
             return response.json();
         })
         .then(function(data) {
-            console.log(data);
+            for (var i = 0; i < 6; i++) {
+                console.log(data.daily[i]);
+            }
         })
 }
 
