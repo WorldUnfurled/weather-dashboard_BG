10bf49a5f55c60d5f09c7e23cc7af3228833cd2a
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-06-29 00:30:55 -0400

    Forgot to mention new dates in last commit; Moved vars to global scope

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,14 @@ var key = '4df499f07bd89e71ed347810c57fb5b1';
 var city;
 var requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely&units=imperial&appid=" + key;
 
+var weather;
+var temp;
+var wind;
+var humidity;
+var uvi;
+
+var forecastObj = {};
+
 
 var getForecast = function() {
     fetch(requestURL)
@@ -9,20 +17,6 @@ var getForecast = function() {
             return response.json();
         })
         .then(function(data) {
-            var weather;
-            var temp;
-            var wind;
-            var humidity;
-            var uvi;
-
-            var forecastObj = {
-                0: [],
-                1: [],
-                2: [],
-                3: [],
-                4: [],
-                5: []
-            };
 
             for (var i = 0; i < 6; i++) {
                 weather = data.daily[i].weather[0].main; //Weather
@@ -30,6 +24,8 @@ var getForecast = function() {
                 wind = data.daily[i].wind_speed; //Wind Speed
                 humidity = data.daily[i].humidity; //Humidity
                 uvi = data.daily[i].uvi; //UV Index
+
+                forecastObj[i] = [];
 
                 forecastObj[i].push(weather);
                 forecastObj[i].push(temp);
